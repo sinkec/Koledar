ae7a665b5bb5ef1972a3b1f1f68e02f045489fdc
sinkec <jazbinsek@gmail.com>
2016-05-07 13:37:35 +0200

    Basic Calendar class - creation

--- Calendar.php
+++ Calendar.php
@@ -1,7 +1,15 @@
 <?php
 
+
+/*07.05.2016 - Razred Calendar bo vseboval podatke ko so dan, mesec, leto, funkcije za izris in delo s samim koledarjem */
 class Calendar
 {
+    private $CurentDate   = null; //datum 
+    private $CurrentYear  = 0;    //leto
+    private $CurrentMonth = 0;    //mesec
+    private $CurrentDay   = 0;    //dan
+    private $DayLabels =  array("Ponedeljek","Torek","Sreda","Četrtek","Petek","Sobota","Nedelja"); //imena dnevov - v enodimenzionalnem polju
+      
 }
 
 ?>
