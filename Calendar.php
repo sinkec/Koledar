2c6f51dae5787c6798f941a848ca1bcbac7e24af
Matej Jazbinšek <jazbinsek@gmail.com>
2016-05-11 19:55:07 +0200

    test of constructor

--- Calendar.php
+++ Calendar.php
@@ -4,11 +4,17 @@
 /*07.05.2016 - Razred Calendar bo vseboval podatke ko so dan, mesec, leto, funkcije za izris in delo s samim koledarjem */
 class Calendar
 {
-    private $CurentDate   = null; //datum 
+    private $CurrentDate   = null; //datum 
     private $CurrentYear  = 0;    //leto
     private $CurrentMonth = 0;    //mesec
     private $CurrentDay   = 0;    //dan
     private $DayLabels =  array("Ponedeljek","Torek","Sreda","Četrtek","Petek","Sobota","Nedelja"); //imena dnevov - v enodimenzionalnem polju
+    
+    public function __construct()
+    {
+        $this->CurrentDate = date('d.m.y');
+        echo $this->CurrentDate;
+    }
       
 }
 
--- index.php
+++ index.php
@@ -16,6 +16,8 @@
  echo "Hello world pa še neki sranja zraven";
  echo "sej se bomo naučil :P";
  echo "Matija was here :D";
+ 
+ $NewCalendar = new Calendar();
  /*06.05.16 Matej: pač to sem nardil zato pa zato če kej ni kul odpiš,... pa bomo zrihtal,.. al ka :D*/
  /*06.05.2016 Matija: Razumljivo */
  /*Sm zej tud pogrunto kok lohka oba delama na istem fajlu :D heheh ti bom razložu*/
